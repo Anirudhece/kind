5c2f99c68828663bb8079838feef9d9087bdf683
Anirudh <anirudhece2020@gmail.com>
2024-05-03 00:41:10 +0530

    form2

--- src/view/form/Form.jsx
+++ src/view/form/Form.jsx
@@ -7,13 +7,16 @@ import {
   Button,
   Flex,
   VStack,
-  HStack,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
 } from "@chakra-ui/react";
 
 const TopInfo = ({ main, sub }) => {
   return (
     <>
-      <Box>
+      <Box mb="10">
         <Heading color="blue.900" as="b" w="100%" mb="2%">
           {main}
         </Heading>
@@ -32,6 +35,75 @@ const Form1 = () => {
         main=" Personal info"
         sub=" Please provide your name, email address and phone number."
       />
+      <Box color="blue.800">
+        <FormControl>
+          <FormLabel htmlFor="name">Name</FormLabel>
+          <Input id="name" placeholder="e.g. Stephen King" />
+        </FormControl>
+
+        <FormControl mt="6">
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input
+            placeholder="e.g. stephenking@lorem.com"
+            id="email"
+            type="email"
+          />
+        </FormControl>
+
+        <FormControl mt="6">
+          <FormLabel htmlFor="phone" mt="2%">
+            Phone Number
+          </FormLabel>
+          <Input id="phone" type="number" placeholder="e.g. +1 234 567 890" />
+        </FormControl>
+      </Box>
+    </>
+  );
+};
+
+const Form2 = () => {
+  const addOns = [
+    {
+      title: "Online Service",
+      description: "access to multiplayer games",
+      price: 1,
+    },
+    {
+      title: "Larger storage",
+      description: "extra 1TB of cloud save",
+      price: 2,
+    },
+    {
+      title: "Customizable profile",
+      description: "custom theme on your profile",
+      price: 2,
+    },
+  ];
+  return (
+    <>
+      <TopInfo
+        main="Select your plan"
+        sub="You have the option for monthly and yearly billing."
+      />
+      <VStack>
+        <Flex
+          p="4"
+          alignItems="center"
+          borderWidth="1px"
+          borderRadius="lg"
+          w="full"
+        >
+          <Flex>
+            <Checkbox  m="2" />
+            <Box m="4">
+              <Text>Online Service</Text>
+              <Text>access to multiplayer games</Text>
+            </Box>
+          </Flex>
+          <Spacer />
+          <Text m="2">+$1/mo</Text>
+        </Flex>
+      </VStack>
     </>
   );
 };
@@ -41,15 +113,25 @@ const Multistep = () => {
     <>
       <VStack h="100vh">
         <Box w="60%" mt="6">
-          <Form1 />
+          {/* <Form1 /> */}
+          <Form2 />
         </Box>
 
         <Spacer />
 
         <Flex mb="10" w="60%">
-          <Button size='lg' color='gray.500' variant='ghost' colorScheme='gray' >Go Back</Button>
+          <Button size="lg" color="gray.500" variant="ghost" colorScheme="gray">
+            Go Back
+          </Button>
           <Spacer />
-          <Button size='lg' bg='blue.900' colorScheme="facebook" variant='solid' >Next</Button>
+          <Button
+            size="lg"
+            bg="blue.900"
+            colorScheme="facebook"
+            variant="solid"
+          >
+            Next
+          </Button>
         </Flex>
       </VStack>
     </>
