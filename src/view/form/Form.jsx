980f2cfe7d08542879d74a269e2f92518583db2b
Anirudh <anirudhece2020@gmail.com>
2024-05-03 14:54:17 +0530

    form 2 done

--- src/view/form/Form.jsx
+++ src/view/form/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Heading,
   Text,
@@ -11,7 +11,12 @@ import {
   FormLabel,
   Input,
   Checkbox,
+  Stack,
 } from "@chakra-ui/react";
+
+import { PiJoystickFill } from "react-icons/pi"; //1
+import { FaGamepad } from "react-icons/fa6"; //2
+import { IoGameController } from "react-icons/io5"; //3
 
 const TopInfo = ({ main, sub }) => {
   return (
@@ -62,6 +67,78 @@ const Form1 = () => {
 };
 
 const Form2 = () => {
+  const [selectedBox, setSelectedBox] = useState(null);
+
+  const data = [
+    {
+      icon: <PiJoystickFill />,
+      title: "Arcade",
+      price: "9",
+      iconBackground: "#eeb488",
+    },
+    {
+      icon: <FaGamepad />,
+      title: "Advanced",
+      price: "12",
+      iconBackground: "#e98690",
+    },
+    {
+      icon: <IoGameController />,
+      title: "Pro",
+      price: "15",
+      iconBackground: "#433cf4",
+    },
+  ];
+
+  const handleBoxClick = (index) => {
+    setSelectedBox(index);
+  };
+
+  return (
+    <>
+      <TopInfo
+        main="Select your plan"
+        sub="You have the option of monthly and yearly billing"
+      />
+
+      <Box display="flex" justifyContent="space-between">
+        {data.map((ele, ind) => (
+          <Box
+            key={ind}
+            w="30%"
+            borderWidth="3px"
+            borderRadius="lg"
+            p="4"
+            bg={selectedBox === ind && "purple.50"}
+            borderColor={selectedBox === ind && "blue.500"} 
+            onClick={() => handleBoxClick(ind)}
+            _hover={{ cursor: "pointer" }} 
+          >
+            <Box display="flex" mb="12">
+              <Text
+                fontSize="2xl"
+                background={ele.iconBackground}
+                p="3"
+                color="white"
+                borderRadius="full"
+              >
+                {ele.icon}
+              </Text>
+            </Box>
+            <Text color="blue.800" fontSize="lg" as="b">
+              {ele.title}
+            </Text>
+            <Text color="gray.500" fontSize="sm" colorScheme="gray">
+              ${ele.price}/mo
+            </Text>
+          </Box>
+        ))}
+      </Box>
+    </>
+  );
+};
+
+const Form3 = () => {
   const addOns = [
     {
       title: "Online Service",
@@ -79,6 +156,18 @@ const Form2 = () => {
       price: 2,
     },
   ];
+
+  // State to manage checked status of checkboxes
+  const [checkedItems, setCheckedItems] = useState({});
+
+  // Function to handle checkbox state change
+  const handleCheckboxChange = (index) => {
+    setCheckedItems({
+      ...checkedItems,
+      [index]: !checkedItems[index],
+    });
+  };
+
   return (
     <>
       <TopInfo
@@ -88,14 +177,21 @@ const Form2 = () => {
       <VStack>
         {addOns.map((ele, ind) => (
           <Flex
+            key={ind}
             p="2"
             alignItems="center"
             borderWidth="1px"
             borderRadius="lg"
             w="full"
+            borderColor={checkedItems[ind] ? "blue.600" : "gray.200"} // Conditionally set border color
           >
             <Flex>
-              <Checkbox border="gray" m="2" />
+              <Checkbox
+                border="gray"
+                m="2"
+                isChecked={checkedItems[ind]}
+                onChange={() => handleCheckboxChange(ind)}
+              />
               <Box mb="2" mt="2" mr="4" ml="4">
                 <Text color="blue.800" fontSize="lg" as="b">
                   {ele.title}
@@ -114,29 +210,35 @@ const Form2 = () => {
   );
 };
 
+const Form4= ()=>{
+  return(
+    <>
+      <TopInfo main='Finishing up' sub='Double check everything looks OK before confirming.' />
+    </>
+  )
+}
+
+
 const Multistep = () => {
   return (
     <>
       <VStack h="100vh">
         <Box w="60%" mt="6">
           {/* <Form1 /> */}
-          <Form2 />
+          {/* <Form2 /> */}
+          {/* <Form3 /> */}
+          <Form4/>
         </Box>
 
         <Spacer />
 
         <Flex mb="10" w="60%">
-          <Button size="lg" color="gray.500" variant="ghost" colorScheme="gray">
+          <Button color="gray.500" variant="ghost" colorScheme="gray">
             Go Back
           </Button>
           <Spacer />
-          <Button
-            size="lg"
-            bg="blue.900"
-            colorScheme="facebook"
-            variant="solid"
-          >
-            Next
+          <Button bg="blue.900" colorScheme="facebook" variant="solid">
+            Next Step
           </Button>
         </Flex>
       </VStack>
--- src/view/form/temp.jsx
+++ src/view/form/temp.jsx
@@ -1,348 +1,92 @@
-'use client'
-
-import { useState } from 'react'
 import {
-  Progress,
   Box,
-  ButtonGroup,
   Button,
-  Heading,
+  Container,
   Flex,
-  FormControl,
-  GridItem,
-  FormLabel,
-  Input,
-  Select,
-  SimpleGrid,
-  InputLeftAddon,
-  InputGroup,
-  Textarea,
-  FormHelperText,
-  InputRightElement,
-} from '@chakra-ui/react'
+  Heading,
+  Icon,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { ReactElement } from "react";
+import {
+  FcAbout,
+  FcAssistant,
+  FcCollaboration,
+  FcDonate,
+  FcManager,
+} from "react-icons/fc";
 
-import { useToast } from '@chakra-ui/react'
-
-const Form1 = () => {
-  const [show, setShow] = useState(false)
-  const handleClick = () => setShow(!show)
+const Card = ({ heading, description, icon, href }) => {
   return (
-    <>
-      <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
-        User Registration
-      </Heading>
-      <Flex>
-        <FormControl mr="5%">
-          <FormLabel htmlFor="first-name" fontWeight={'normal'}>
-            First name
-          </FormLabel>
-          <Input id="first-name" placeholder="First name" />
-        </FormControl>
+    <Box
+      maxW={{ base: "full", md: "275px" }}
+      w={"full"}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      p={5}
+    >
+      <Stack align={"start"} spacing={2}>
+        <Flex
+          w={16}
+          h={16}
+          align={"center"}
+          justify={"center"}
+          color={"white"}
+          rounded={"full"}
+          bg={useColorModeValue("gray.100", "gray.700")}
+        >
+          {icon}
+        </Flex>
+        <Box mt={2}>
+          <Heading size="md">{heading}</Heading>
+          <Text mt={1} fontSize={"sm"}>
+            {description}
+          </Text>
+        </Box>
+        <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
+          Learn more
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
 
-        <FormControl>
-          <FormLabel htmlFor="last-name" fontWeight={'normal'}>
-            Last name
-          </FormLabel>
-          <Input id="last-name" placeholder="First name" />
-        </FormControl>
-      </Flex>
-      <FormControl mt="2%">
-        <FormLabel htmlFor="email" fontWeight={'normal'}>
-          Email address
-        </FormLabel>
-        <Input id="email" type="email" />
-        <FormHelperText>We&apos;ll never share your email.</FormHelperText>
-      </FormControl>
+export default function gridListWith() {
+  return (
+    <Box p={4}>
 
-      <FormControl>
-        <FormLabel htmlFor="password" fontWeight={'normal'} mt="2%">
-          Password
-        </FormLabel>
-        <InputGroup size="md">
-          <Input
-            pr="4.5rem"
-            type={show ? 'text' : 'password'}
-            placeholder="Enter password"
+
+      <Container maxW={"5xl"} mt={12}>
+        <Flex flexWrap="wrap" gridGap={6} justify="center">
+          <Card
+            heading={"Heading"}
+            icon={<Icon as={FcAssistant} w={10} h={10} />}
+            description={
+              "Lorem ipsum dolor sit amet catetur, adipisicing elit."
+            }
+            href={"#"}
           />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-    </>
-  )
-}
-
-const Form2 = () => {
-  return (
-    <>
-      <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
-        User Details
-      </Heading>
-      <FormControl as={GridItem} colSpan={[6, 3]}>
-        <FormLabel
-          htmlFor="country"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}>
-          Country / Region
-        </FormLabel>
-        <Select
-          id="country"
-          name="country"
-          autoComplete="country"
-          placeholder="Select option"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md">
-          <option>United States</option>
-          <option>Canada</option>
-          <option>Mexico</option>
-        </Select>
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={6}>
-        <FormLabel
-          htmlFor="street_address"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          Street address
-        </FormLabel>
-        <Input
-          type="text"
-          name="street_address"
-          id="street_address"
-          autoComplete="street-address"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
-        <FormLabel
-          htmlFor="city"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          City
-        </FormLabel>
-        <Input
-          type="text"
-          name="city"
-          id="city"
-          autoComplete="city"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-        <FormLabel
-          htmlFor="state"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          State / Province
-        </FormLabel>
-        <Input
-          type="text"
-          name="state"
-          id="state"
-          autoComplete="state"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-        <FormLabel
-          htmlFor="postal_code"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          ZIP / Postal
-        </FormLabel>
-        <Input
-          type="text"
-          name="postal_code"
-          id="postal_code"
-          autoComplete="postal-code"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-    </>
-  )
-}
-
-const Form3 = () => {
-  return (
-    <>
-      <Heading w="100%" textAlign={'center'} fontWeight="normal">
-        Social Handles
-      </Heading>
-      <SimpleGrid columns={1} spacing={6}>
-        <FormControl as={GridItem} colSpan={[3, 2]}>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="md"
-            color="gray.700"
-            _dark={{
-              color: 'gray.50',
-            }}>
-            Website
-          </FormLabel>
-          <InputGroup size="sm">
-            <InputLeftAddon
-              bg="gray.50"
-              _dark={{
-                bg: 'gray.800',
-              }}
-              color="gray.500"
-              rounded="md">
-              http://
-            </InputLeftAddon>
-            <Input
-              type="tel"
-              placeholder="www.example.com"
-              focusBorderColor="brand.400"
-              rounded="md"
-            />
-          </InputGroup>
-        </FormControl>
-
-        <FormControl id="email" mt={1}>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="md"
-            color="gray.700"
-            _dark={{
-              color: 'gray.50',
-            }}>
-            About
-          </FormLabel>
-          <Textarea
-            placeholder="you@example.com"
-            rows={3}
-            shadow="sm"
-            focusBorderColor="brand.400"
-            fontSize={{
-              sm: 'sm',
-            }}
+          <Card
+            heading={"Heading"}
+            icon={<Icon as={FcCollaboration} w={10} h={10} />}
+            description={
+              "Lorem ipsum dolor sit amet catetur, adipisicing elit."
+            }
+            href={"#"}
           />
-          <FormHelperText>
-            Brief description for your profile. URLs are hyperlinked.
-          </FormHelperText>
-        </FormControl>
-      </SimpleGrid>
-    </>
-  )
-}
-
-export default function Multistep() {
-  const toast = useToast()
-  const [step, setStep] = useState(1)
-  const [progress, setProgress] = useState(33.33)
-
-  return (
-    <>
-      <Box
-        borderWidth="1px"
-        rounded="lg"
-        shadow="1px 1px 3px rgba(0,0,0,0.3)"
-        maxWidth={800}
-        p={6}
-        m="10px auto"
-        as="form">
-        <Progress hasStripe value={progress} mb="5%" mx="5%" isAnimated></Progress>
-        {step === 1 ? <Form1 /> : step === 2 ? <Form2 /> : <Form3 />}
-        <ButtonGroup mt="5%" w="100%">
-          <Flex w="100%" justifyContent="space-between">
-            <Flex>
-              <Button
-                onClick={() => {
-                  setStep(step - 1)
-                  setProgress(progress - 33.33)
-                }}
-                isDisabled={step === 1}
-                colorScheme="teal"
-                variant="solid"
-                w="7rem"
-                mr="5%">
-                Back
-              </Button>
-              <Button
-                w="7rem"
-                isDisabled={step === 3}
-                onClick={() => {
-                  setStep(step + 1)
-                  if (step === 3) {
-                    setProgress(100)
-                  } else {
-                    setProgress(progress + 33.33)
-                  }
-                }}
-                colorScheme="teal"
-                variant="outline">
-                Next
-              </Button>
-            </Flex>
-            {step === 3 ? (
-              <Button
-                w="7rem"
-                colorScheme="red"
-                variant="solid"
-                onClick={() => {
-                  toast({
-                    title: 'Account created.',
-                    description: "We've created your account for you.",
-                    status: 'success',
-                    duration: 3000,
-                    isClosable: true,
-                  })
-                }}>
-                Submit
-              </Button>
-            ) : null}
-          </Flex>
-        </ButtonGroup>
-      </Box>
-    </>
-  )
+          <Card
+            heading={"Heading"}
+            icon={<Icon as={FcDonate} w={10} h={10} />}
+            description={
+              "Lorem ipsum dolor sit amet catetur, adipisicing elit."
+            }
+            href={"#"}
+          />
+        </Flex>
+      </Container>
+    </Box>
+  );
 }
