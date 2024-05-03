8aec8df4ef48811f150a913f4b1c27d8d32ab8d4
Anirudh <anirudhece2020@gmail.com>
2024-05-03 21:12:08 +0530

    form2

--- src/view/step-form/forms/Form2.jsx
+++ src/view/step-form/forms/Form2.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import TopInfo from "../component/TopInfo";
-import { Text, Box } from "@chakra-ui/react";
+import { Text, Box, Switch, Flex, ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { PiJoystickFill } from "react-icons/pi"; //1
 import { FaGamepad } from "react-icons/fa6"; //2
 import { IoGameController } from "react-icons/io5"; //3
+
+// Extend the theme to customize the Switch component
+const theme = extendTheme({
+  components: {
+    Switch: {
+      baseStyle: {
+        track: {
+          backgroundColor: "blue.800",
+        },
+      },
+    },
+  },
+});
 
 const Form2 = () => {
   const [selectedBox, setSelectedBox] = useState(null);
@@ -34,46 +47,62 @@ const Form2 = () => {
   };
 
   return (
-    <>
-      <TopInfo
-        main="Select your plan"
-        sub="You have the option of monthly and yearly billing"
-      />
+    <ChakraProvider theme={theme}>
+      <>
+        <TopInfo
+          main="Select your plan"
+          sub="You have the option of monthly and yearly billing"
+        />
 
-      <Box display="flex" justifyContent="space-between">
-        {data.map((ele, ind) => (
-          <Box
-            key={ind}
-            w="30%"
-            borderWidth="3px"
-            borderRadius="lg"
-            p="4"
-            bg={selectedBox === ind && "purple.50"}
-            borderColor={selectedBox === ind && "blue.500"}
-            onClick={() => handleBoxClick(ind)}
-            _hover={{ cursor: "pointer" }}
-          >
-            <Box display="flex" mb="12">
-              <Text
-                fontSize="2xl"
-                background={ele.iconBackground}
-                p="3"
-                color="white"
-                borderRadius="full"
-              >
-                {ele.icon}
+        <Box display="flex" justifyContent="space-between">
+          {data.map((ele, ind) => (
+            <Box
+              key={ind}
+              w="30%"
+              borderWidth="3px"
+              borderRadius="lg"
+              p="4"
+              bg={selectedBox === ind && "green.50"}
+              borderColor={selectedBox === ind && "blue.500"}
+              onClick={() => handleBoxClick(ind)}
+              _hover={{ cursor: "pointer" }}
+            >
+              <Box display="flex" mb="12">
+                <Text
+                  fontSize="2xl"
+                  background={ele.iconBackground}
+                  p="3"
+                  color="white"
+                  borderRadius="full"
+                >
+                  {ele.icon}
+                </Text>
+              </Box>
+              <Text color="blue.800" fontSize="lg" as="b">
+                {ele.title}
+              </Text>
+              <Text color="gray.500" fontSize="sm" colorScheme="gray">
+                ${ele.price}/mo
               </Text>
             </Box>
-            <Text color="blue.800" fontSize="lg" as="b">
-              {ele.title}
-            </Text>
-            <Text color="gray.500" fontSize="sm" colorScheme="gray">
-              ${ele.price}/mo
-            </Text>
-          </Box>
-        ))}
-      </Box>
-    </>
+          ))}
+        </Box>
+        <Flex
+          alignItems="center"
+          color="blue.800"
+          fontSize="md"
+          as="b"
+          mt="8"
+          p="4"
+          bg="blue.50"
+          justifyContent="center"
+        >
+          <Text>Monthly</Text>
+          <Switch size="lg" pl="2" pr="2" />
+          <Text>Yearly</Text>
+        </Flex>
+      </>
+    </ChakraProvider>
   );
 };
 export default Form2;
